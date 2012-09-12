322916784e76d5744a797ed3fc6bb9d13a897c8d
Matt Dietz <matt.dietz@rackspace.com>
2012-09-12 18:57:52 +0000

    Still getting the hang of pypi

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import setuptools
 
 setuptools.setup(
     name='os_networksv2_python_novaclient_ext',
-    version='0.2',
+    version='0.21',
     description='Adds rackspace networks support to python-novaclient',
     long_description=open('README.rst').read(),
     author='Rackspace',
